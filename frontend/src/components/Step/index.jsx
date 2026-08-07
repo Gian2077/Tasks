566c510186534db16ece26184f773b1153bda068
Gian2077 <gianrodrigues.2077@gmail.com>
2026-08-07 18:26:24 -0300

    bugfix(css): fix steps overflow issue

--- frontend/src/components/Step/index.jsx
+++ frontend/src/components/Step/index.jsx
@@ -1,3 +1,4 @@
+import { useLayoutEffect, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { toggleStep, deleteStep } from "../../store/slices/steps/stepSlice";
 import { ButtonStepToggleStatus } from "../ButtonStepToggleStatus";
@@ -5,11 +6,45 @@ import styles from "./Step.module.css";
 import { ButtonStepDelete } from "../ButtonStepDelete";
 export function Step({ step }) {
   const dispatch = useDispatch();
+  const stepRef = useRef(null);
+  const textRef = useRef(null);
+  const [isOverflowing, setIsOverflowing] = useState(false);
+  const checkOverflow = (text) => {
+    const overflowing = text.scrollWidth > text.clientWidth;
+    setIsOverflowing(overflowing);
+  };
+  const calcOverflow = (text) => {
+    return text.scrollWidth - text.clientWidth;
+  };
+  useLayoutEffect(() => {
+    const step = stepRef.current;
+    if (!step) return;
+    const text = textRef.current;
+    checkOverflow(text);
+    step.style.setProperty(
+      "--overflow-width",
+      `-${calcOverflow(text) + 10}cqw`,
+    );
+    step.style.setProperty(
+      "--animation-speed",
+      `${calcOverflow(text) / 30 > 2 ? calcOverflow(text) / 30 : 5}s`,
+    );
+    window.addEventListener("resize", checkOverflow);
+    return window.removeEventListener("resize", checkOverflow);
+  }, [step.title]);
   return (
     <>
-      <li className={`${styles.li} ${step.completed ? styles.completed : ""}`}>
+      <li
+        className={`${styles.li} ${step.completed ? styles.completed : ""}`}
+        ref={stepRef}
+      >
         <div className={styles.wrapper}>
-          <h4>{step.title}</h4>
+          <h4
+            className={`${styles.title} ${isOverflowing ? styles.overflowing : ""}`}
+            ref={textRef}
+          >
+            {step.title}
+          </h4>
         </div>
         <div className={styles.actions}>
           <ButtonStepToggleStatus
